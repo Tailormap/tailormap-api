34566f8c665dc1f87d006764fc4e572561dbbc58
Mark Prins <1165786+mprins@users.noreply.github.com>
2021-11-11 18:31:03 +0100

    add some javadoc

--- src/main/java/nl/b3p/tailormap/api/TailormapApiApplication.java
+++ src/main/java/nl/b3p/tailormap/api/TailormapApiApplication.java
@@ -3,9 +3,19 @@ package nl.b3p.tailormap.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * API application starter.
+ *
+ * @since 0.1
+ */
 @SpringBootApplication
 public class TailormapApiApplication {
 
+    /**
+     * default starter method.
+     *
+     * @param args an array of arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(TailormapApiApplication.class, args);
     }
--- src/test/java/nl/b3p/tailormap/api/TailormapApiApplicationTests.java
+++ src/test/java/nl/b3p/tailormap/api/TailormapApiApplicationTests.java
@@ -3,8 +3,13 @@ package nl.b3p.tailormap.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * testcases for {@link TailormapApiApplication}.
+ *
+ * @since 0.1
+ */
 @SpringBootTest
-class TailormapApiApplicationTests {
+public class TailormapApiApplicationTests {
 
     @Test
     void contextLoads() {}
