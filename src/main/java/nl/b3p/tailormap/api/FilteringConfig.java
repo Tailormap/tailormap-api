99f368ac1cb10b5a8a4327c870de7b82f2303c5a
Mark Prins <1165786+mprins@users.noreply.github.com>
2021-12-17 09:10:45 +0100

    [HTM-66] Add FilterRegistrationBean to handle proxy headers

---
+++ src/main/java/nl/b3p/tailormap/api/FilteringConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2021 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+package nl.b3p.tailormap.api;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+import java.util.List;
+
+import javax.servlet.DispatcherType;
+
+/**
+ * (Servlet) filtering config.
+ *
+ * @author mprins
+ * @since 0.1
+ */
+@Configuration
+public class FilteringConfig {
+
+    /**
+     * Handle X-Forwarded-for and other proxy headers.
+     *
+     * @return the configured filtering bean
+     */
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        FilterRegistrationBean<ForwardedHeaderFilter> registration =
+                new FilterRegistrationBean<>(filter);
+        registration.setDispatcherTypes(
+                DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registration.setUrlPatterns(List.of("/*"));
+        return registration;
+    }
+}
