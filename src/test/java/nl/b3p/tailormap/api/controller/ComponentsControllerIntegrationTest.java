da9eabad673763c4da16b3cd0d7017a5830fc641
Mark Prins <1165786+mprins@users.noreply.github.com>
2021-12-27 12:47:29 +0100

    [HTM-70] initial components endpoint
    only returns a dummy component for now
    
    resolves HTM-70

---
+++ src/test/java/nl/b3p/tailormap/api/controller/ComponentsControllerIntegrationTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2021 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+package nl.b3p.tailormap.api.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import nl.b3p.tailormap.api.HSQLDBTestProfileJPAConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(classes = {HSQLDBTestProfileJPAConfiguration.class, ComponentsController.class})
+@AutoConfigureMockMvc
+@EnableAutoConfiguration
+@ActiveProfiles("test")
+class ComponentsControllerIntegrationTest {
+    @Autowired private MockMvc mockMvc;
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void should_return_data_for_configured_app() throws Exception {
+        mockMvc.perform(get("/components/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0]").isMap())
+                .andExpect(jsonPath("$[0].type").value("Dummy"))
+                .andExpect(jsonPath("$[0].config").isMap());
+    }
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void should_error_when_calling_with_nonexistent_id() throws Exception {
+        mockMvc.perform(get("/components/400"))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(
+                        jsonPath("$.message")
+                                .value("Requested an application that does not exist"));
+    }
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void should_not_find_when_called_without_id() throws Exception {
+        mockMvc.perform(get("/components/")).andExpect(status().isNotFound());
+    }
+}
