7e272048e79eff6f400322ba2c60e3fd2fb37e7e
Mark Prins <1165786+mprins@users.noreply.github.com>
2022-03-14 12:27:48 +0100

    [HTM-251] VersionController check and update responses for dummy data

--- src/test/java/nl/b3p/tailormap/api/controller/VersionControllerIntegrationTest.java
+++ src/test/java/nl/b3p/tailormap/api/controller/VersionControllerIntegrationTest.java
@@ -10,7 +10,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 import nl.b3p.tailormap.api.HSQLDBTestProfileJPAConfiguration;
+import nl.b3p.tailormap.api.repository.MetadataRepository;
+import nl.tailormap.viewer.config.metadata.Metadata;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +26,7 @@ import java.util.Map;
 @ActiveProfiles("test")
 class VersionControllerIntegrationTest {
     @Autowired private VersionController versionController;
+    @Autowired private MetadataRepository metadataRepository;
 
     @Test
     void testGetVersion() {
@@ -48,6 +52,33 @@ class VersionControllerIntegrationTest {
                         projectVersion,
                         "databaseversion",
                         databaseVersion,
+                        "apiVersion",
+                        apiVersion);
+
+        assertEquals(expected, versionController.getVersion(), "Unexpected version response.");
+    }
+
+    @Test
+    /* this test changes database content */
+    @Order(Integer.MAX_VALUE)
+    void unknown_database_version() throws Exception {
+        metadataRepository.deleteMetadataByConfigKey(Metadata.DATABASE_VERSION_KEY);
+
+        String projectVersion = System.getProperty("project.version");
+        assumeFalse(
+                null == projectVersion,
+                "Project version unknown, should be set as system property");
+
+        String apiVersion = System.getenv("API_VERSION");
+        assumeFalse(null == apiVersion, "API version unknown, should be set in system environment");
+
+        assertNotNull(versionController.getVersion(), "Version info not found");
+        Map<String, String> expected =
+                Map.of(
+                        "version",
+                        projectVersion,
+                        "databaseversion",
+                        "unknown",
                         "apiVersion",
                         apiVersion);
 
