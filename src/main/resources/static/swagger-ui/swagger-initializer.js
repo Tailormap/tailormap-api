40e93d10d0874938be1575eff9fe6be85ada5650
Matthijs Laan <matthijsln@gmail.com>
2022-04-05 17:45:09 +0200

    Send XSRF token

--- src/main/resources/static/swagger-ui/swagger-initializer.js
+++ src/main/resources/static/swagger-ui/swagger-initializer.js
@@ -10,6 +10,13 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    requestInterceptor: (request) => {
+      const match = document.cookie.match(/XSRF-TOKEN=([0-9a-f-]+)/);
+      if(match) {
+        request.headers['X-XSRF-TOKEN'] = match[1];
+      }
+      return request;
+    }
   });
 };
