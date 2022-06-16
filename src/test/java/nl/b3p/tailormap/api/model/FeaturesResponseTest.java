93b265f012973bc6e375accb0af122d6b5adebbf
Mark Prins <1165786+mprins@users.noreply.github.com>
2022-06-16 14:39:41 +0200

    [HTM-364] API design for paged features response (#127)
    * [HTM-364] Initial design of the API for paged feature responses and a testcase decribing default behaviour
    
    * [HTM-364] remove unnecessary all parameter from request, just ask for the first page

---
+++ src/test/java/nl/b3p/tailormap/api/model/FeaturesResponseTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2022 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+package nl.b3p.tailormap.api.model;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+class FeaturesResponseTest {
+    @Test
+    void testRequiredFieldsNotNull() {
+        FeaturesResponse fr = new FeaturesResponse();
+        assertNotNull(fr.getFeatures(), "featurelist should not be null");
+        assertNotNull(fr.getColumnMetadata(), "columnMetadata should not be null");
+        assertNull(fr.getPage(), "default page should be null");
+        assertNull(fr.getPageSize(), "default pageSize should be null");
+        assertNull(fr.getTotal(), "default total should be null");
+    }
+}
