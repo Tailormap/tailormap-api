5b637f71ef887931a6b3210b6eee6bfcd06d2a05
Mark Prins <1165786+mprins@users.noreply.github.com>
2022-06-21 14:14:34 +0200

    [HTM-391] Set-up integration test data for CI/CD (#135)
    * [HTM-391] re-enable database integration tests for featurescontroller
    
    * [HTM-391] gisdata databases startup
    
    use a compose file for the gisdata databases
    
    use a shell script to setup test databases

---
+++ build/ci/testdata-setup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Copyright (C) 2022 B3Partners B.V.
+#
+# SPDX-License-Identifier: MIT
+set -e
+
+docker compose -f ./build/ci/docker-compose.yml up -d
+
+POSTGIS_HEALTHY=$(docker inspect --format="{{.State.Health.Status}}" postgis)
+ORACLE_HEALTHY=$(docker inspect --format="{{.State.Health.Status}}" oracle)
+SQLSERVER_HEALTHY=$(docker inspect --format="{{.State.Health.Status}}" sqlserver)
+_WAIT=0;
+
+while :
+do
+  printf " $_WAIT"
+  if [ "$POSTGIS_HEALTHY" == "healthy" ] && [ "$ORACLE_HEALTHY" == "healthy" ] && [ "$SQLSERVER_HEALTHY" == "healthy" ]; then
+    break
+  fi
+
+  sleep 10
+
+  _WAIT=$(($_WAIT+10))
+  POSTGIS_HEALTHY=$(docker inspect --format="{{.State.Health.Status}}" postgis)
+  ORACLE_HEALTHY=$(docker inspect --format="{{.State.Health.Status}}" oracle)
+  SQLSERVER_HEALTHY=$(docker inspect --format="{{.State.Health.Status}}" sqlserver)
+
+done
+
+docker logs postgis
+docker inspect postgis
+docker logs oracle
+docker inspect oracle
+docker logs sqlserver
+docker inspect sqlserver
+
+# update host/port numbers
+psql -h 127.0.0.1 -p 5432 -d tailormap -a -c "update public.feature_source set url = replace(url,'\"host\":\"postgis\"','\"host\":\"127.0.0.1\"') where protocol = 'jdbc';"
+psql -h 127.0.0.1 -p 5432 -d tailormap -a -c "update public.feature_source set url = replace(url,'\"port\":\"5432\"','\"port\":\"54322\"') where protocol = 'jdbc';"
+psql -h 127.0.0.1 -p 5432 -d tailormap -a -c "update public.feature_source set url = replace(url,'\"host\":\"oracle\"','\"host\":\"127.0.0.1\"') where protocol = 'jdbc';"
+psql -h 127.0.0.1 -p 5432 -d tailormap -a -c "update public.feature_source set url = replace(url,'\"host\":\"sqlserver\"','\"host\":\"127.0.0.1\"') where protocol = 'jdbc';"
