0a8964a3fdceb27bb0070662abb3f5de081cfb59
Mark Prins <1165786+mprins@users.noreply.github.com>
2022-09-28 16:18:50 +0200

    Add build date/time to version info so we can easily see if the deployment was updated on a snapshot

--- src/test/java/nl/b3p/tailormap/api/controller/VersionControllerIntegrationTest.java
+++ src/test/java/nl/b3p/tailormap/api/controller/VersionControllerIntegrationTest.java
@@ -13,12 +13,16 @@ import nl.b3p.tailormap.api.HSQLDBTestProfileJPAConfiguration;
 import nl.b3p.tailormap.api.repository.MetadataRepository;
 import nl.tailormap.viewer.config.metadata.Metadata;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 /** Testcases for {@link VersionController}. */
@@ -28,28 +32,46 @@ class VersionControllerIntegrationTest {
     @Autowired private VersionController versionController;
     @Autowired private MetadataRepository metadataRepository;
 
+    private String databaseVersion;
+    private String apiVersion;
+    private String commitSha;
+    private long buildDate;
+    private String projectVersion;
+
+    @BeforeEach
+    void readEnvironment() {
+        projectVersion = System.getProperty("project.version");
+        assumeFalse(
+                null == projectVersion,
+                "Project version unknown, should be set as system property");
+        databaseVersion = System.getenv("DATABASE_VERSION");
+        assumeFalse(
+                null == databaseVersion,
+                "Database version unknown, should be set in system environment");
+
+        apiVersion = System.getenv("API_VERSION");
+        assumeFalse(null == apiVersion, "API version unknown, should be set in system environment");
+
+        commitSha = System.getenv("GIT_COMMIT_ID");
+        assumeFalse(null == commitSha, "Git commit unknown, should be set in system environment");
+
+        assumeFalse(
+                null == System.getenv("BUILD_DATE"),
+                "Build date unknown, should be set in system environment");
+
+        buildDate =
+                LocalDateTime.parse(
+                                System.getenv("BUILD_DATE"), DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                        .toEpochSecond(ZoneOffset.UTC);
+    }
+
     @Test
     void testGetVersion() {
         assertNotNull(
                 versionController, "versionController can not be `null` if Spring Boot works");
         assertNotNull(versionController.getVersion(), "Version info not found");
 
-        String projectVersion = System.getProperty("project.version");
-        assumeFalse(
-                null == projectVersion,
-                "Project version unknown, should be set as system property");
-        String databaseVersion = System.getenv("DATABASE_VERSION");
-        assumeFalse(
-                null == databaseVersion,
-                "Database version unknown, should be set in system environment");
-
-        String apiVersion = System.getenv("API_VERSION");
-        assumeFalse(null == apiVersion, "API version unknown, should be set in system environment");
-
-        String commitSha = System.getenv("GIT_COMMIT_ID");
-        assumeFalse(null == commitSha, "Git commit unknown, should be set in system environment");
-
-        Map<String, String> expected =
+        Map<String, Object> expected =
                 Map.of(
                         "version",
                         projectVersion,
@@ -58,9 +80,34 @@ class VersionControllerIntegrationTest {
                         "apiVersion",
                         apiVersion,
                         "commitSha",
-                        commitSha);
+                        commitSha,
+                        "buildDate",
+                        buildDate);
 
-        assertEquals(expected, versionController.getVersion(), "Unexpected version response.");
+        assertEquals(
+                expected.keySet(),
+                versionController.getVersion().keySet(),
+                "Unexpected version response, some keys are missing.");
+
+        // buildDate can be a few seconds off when running integration tests
+        assertEquals(
+                buildDate,
+                LocalDateTime.parse(
+                                versionController.getVersion().get("buildDate"),
+                                DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                        .toEpochSecond(ZoneOffset.UTC),
+                10,
+                "buildDate should almost be the same");
+
+        expected.entrySet().stream()
+                .filter(e -> !e.getKey().equals("buildDate"))
+                .forEach(
+                        e -> {
+                            assertEquals(
+                                    e.getValue(),
+                                    versionController.getVersion().get(e.getKey()),
+                                    () -> "Unexpected response for: " + e.getKey());
+                        });
     }
 
     @Test
@@ -69,19 +116,8 @@ class VersionControllerIntegrationTest {
     void unknown_database_version() {
         metadataRepository.deleteMetadataByConfigKey(Metadata.DATABASE_VERSION_KEY);
 
-        String projectVersion = System.getProperty("project.version");
-        assumeFalse(
-                null == projectVersion,
-                "Project version unknown, should be set as system property");
-
-        String apiVersion = System.getenv("API_VERSION");
-        assumeFalse(null == apiVersion, "API version unknown, should be set in system environment");
-
-        String commitSha = System.getenv("GIT_COMMIT_ID");
-        assumeFalse(null == commitSha, "Git commit unknown, should be set in system environment");
-
         assertNotNull(versionController.getVersion(), "Version info not found");
-        Map<String, String> expected =
+        Map<String, Object> expected =
                 Map.of(
                         "version",
                         projectVersion,
@@ -90,8 +126,32 @@ class VersionControllerIntegrationTest {
                         "apiVersion",
                         apiVersion,
                         "commitSha",
-                        commitSha);
+                        commitSha,
+                        "buildDate",
+                        buildDate);
 
-        assertEquals(expected, versionController.getVersion(), "Unexpected version response.");
+        assertEquals(
+                expected.keySet(),
+                versionController.getVersion().keySet(),
+                "Unexpected version response, some keys are missing.");
+
+        assertEquals(
+                buildDate,
+                LocalDateTime.parse(
+                                versionController.getVersion().get("buildDate"),
+                                DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                        .toEpochSecond(ZoneOffset.UTC),
+                10,
+                "buildDate should almost be the same - there might be a temporal abberation");
+
+        expected.entrySet().stream()
+                .filter(e -> !e.getKey().equals("buildDate"))
+                .forEach(
+                        e -> {
+                            assertEquals(
+                                    e.getValue(),
+                                    versionController.getVersion().get(e.getKey()),
+                                    () -> "Unexpected response for: " + e.getKey());
+                        });
     }
 }
