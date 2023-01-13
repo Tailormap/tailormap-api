1288bc8a9c2ea694c7fae8ef9fccacd578c98e7d
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-01-13 14:04:04 +0100

    HTM-570: Add /user endpoint to get user login information

---
+++ src/test/java/nl/b3p/tailormap/api/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2021 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+package nl.b3p.tailormap.api.controller;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import nl.b3p.tailormap.api.JPAConfiguration;
+import nl.b3p.tailormap.api.security.AuthorizationService;
+import nl.b3p.tailormap.api.security.SecurityConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+/** Testcases for {@link UserController}. */
+@SpringBootTest(
+        classes = {
+            JPAConfiguration.class,
+            SecurityConfig.class,
+            AuthorizationService.class,
+            UserController.class,
+        })
+@AutoConfigureMockMvc
+@EnableAutoConfiguration
+@ActiveProfiles("test")
+class UserControllerIntegrationTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @Autowired UserController userController;
+
+    @Test
+    void testUnauthenticatedGetUser() throws Exception {
+        assertNotNull(userController, "userController can not be `null` if Spring Boot works");
+
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.isAuthenticated").value(false))
+                .andExpect(jsonPath("$.username").value(""));
+    }
+
+    @Test
+    @WithMockUser(
+            username = "admin",
+            authorities = {"Admin"})
+    void testAuthenticatedGetUser() throws Exception {
+        assertNotNull(userController, "userController can not be `null` if Spring Boot works");
+
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.isAuthenticated").value(true))
+                .andExpect(jsonPath("$.username").value("admin"));
+    }
+}
