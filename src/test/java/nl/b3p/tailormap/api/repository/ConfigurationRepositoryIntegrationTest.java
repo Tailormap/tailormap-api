71b99f35ddbad77f65774599b10f7dccd9c37cd9
Matthijs Laan <matthijsln@gmail.com>
2023-02-03 15:34:37 +0100

    Remove rule

--- src/test/java/nl/b3p/tailormap/api/repository/ConfigurationRepositoryIntegrationTest.java
+++ src/test/java/nl/b3p/tailormap/api/repository/ConfigurationRepositoryIntegrationTest.java
@@ -5,6 +5,11 @@
  */
 package nl.b3p.tailormap.api.repository;
 
+import static nl.b3p.tailormap.api.persistence.Configuration.DEFAULT_APP;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import nl.b3p.tailormap.api.annotation.PostgresIntegrationTest;
 import nl.b3p.tailormap.api.persistence.Configuration;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -13,16 +18,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static nl.b3p.tailormap.api.persistence.Configuration.DEFAULT_APP;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
-
-//@SpringBootTest(classes = {JPAConfiguration.class, DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, GeoServiceRepository.class})
-//@EnableJpaRepositories(basePackages = {"nl.b3p.tailormap.api.repository"})
-//@EntityScan(basePackages = {"nl.b3p.tailormap.api.persistence"})
-//@ActiveProfiles("postgresql")
 @PostgresIntegrationTest
 @TestMethodOrder(OrderAnnotation.class)
 class ConfigurationRepositoryIntegrationTest {
@@ -33,7 +28,7 @@ class ConfigurationRepositoryIntegrationTest {
   @Order(1)
   void it_should_findByConfigKeyDefaultApplication() {
     final Configuration c = configurationRepository.findByKey(DEFAULT_APP);
-    assertNotNull(c, "we should have found something");
+    assertNotNull(c);
     assertEquals("default", c.getValue());
   }
 
@@ -42,6 +37,6 @@ class ConfigurationRepositoryIntegrationTest {
   void it_should_not_find_value_after_deleting_key() {
     configurationRepository.deleteConfigurationByKey(DEFAULT_APP);
     final Configuration c = configurationRepository.findByKey(DEFAULT_APP);
-    assertNull(c, "we should not have found anything");
+    assertNull(c);
   }
 }
