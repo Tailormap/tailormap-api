ea92e6bcd007083ea3f0e0d76776615602827848
Matthijs Laan <matthijsln@gmail.com>
2023-02-09 12:29:36 +0100

    Add users and groups

---
+++ src/main/java/nl/b3p/tailormap/api/persistence/Group.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2023 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+package nl.b3p.tailormap.api.persistence;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "groups")
+public class Group {
+  public static final String ADMIN = "admin";
+  public static final String ADMIN_GEO_SERVICES = "admin-geo-services";
+  public static final String ADMIN_USERS = "admin-users";
+  public static final String ADMIN_APPLICATIONS = "admin-applications";
+
+  @Id private String name;
+
+  private boolean systemGroup;
+
+  private String title;
+
+  @Column(columnDefinition = "text")
+  private String notes;
+
+  @ManyToMany(mappedBy = "groups")
+  private Set<User> members = new HashSet<>();
+
+  public String getName() {
+    return name;
+  }
+
+  public Group setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public boolean isSystemGroup() {
+    return systemGroup;
+  }
+
+  public Group setSystemGroup(boolean systemGroup) {
+    this.systemGroup = systemGroup;
+    return this;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public Group setTitle(String title) {
+    this.title = title;
+    return this;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public Group setNotes(String notes) {
+    this.notes = notes;
+    return this;
+  }
+
+  public Set<User> getMembers() {
+    return members;
+  }
+
+  public Group setMembers(Set<User> members) {
+    this.members = members;
+    return this;
+  }
+}
---
+++ src/main/java/nl/b3p/tailormap/api/persistence/User.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2023 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+package nl.b3p.tailormap.api.persistence;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id private String username;
+
+  private String passwordHash;
+
+  private String email;
+
+  private String name;
+
+  @Column(columnDefinition = "text")
+  private String notes;
+
+  @Type(type = "io.hypersistence.utils.hibernate.type.json.JsonBinaryType")
+  @Column(columnDefinition = "jsonb")
+  private JsonNode additionalProperties;
+
+  @ManyToMany
+  @JoinTable(
+      name = "user_groups",
+      joinColumns = @JoinColumn(name = "username"),
+      inverseJoinColumns = @JoinColumn(name = "group_name"))
+  private Set<Group> groups = new HashSet<>();
+
+  public String getUsername() {
+    return username;
+  }
+
+  public User setUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public String getPasswordHash() {
+    return passwordHash;
+  }
+
+  public User setPasswordHash(String passwordHash) {
+    this.passwordHash = passwordHash;
+    return this;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public User setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public User setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public User setNotes(String notes) {
+    this.notes = notes;
+    return this;
+  }
+
+  public JsonNode getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  public User setAdditionalProperties(JsonNode additionalProperties) {
+    this.additionalProperties = additionalProperties;
+    return this;
+  }
+
+  public Set<Group> getGroups() {
+    return groups;
+  }
+
+  public User setGroups(Set<Group> groups) {
+    this.groups = groups;
+    return this;
+  }
+}
