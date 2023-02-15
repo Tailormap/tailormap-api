856d262e125307609a574bd7d283fdda45c4217e
Matthijs Laan <matthijsln@gmail.com>
2023-02-15 21:52:50 +0100

    Load capabilities on create and save

---
+++ src/main/java/nl/b3p/tailormap/api/repository/events/GeoServiceEventHandler.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+package nl.b3p.tailormap.api.repository.events;
+
+import java.lang.invoke.MethodHandles;
+import nl.b3p.tailormap.api.persistence.GeoService;
+import nl.b3p.tailormap.api.persistence.helper.GeoServiceHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+@RepositoryEventHandler
+@Component
+public class GeoServiceEventHandler {
+  private static final Logger logger =
+      LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  private final GeoServiceHelper geoServiceHelper;
+
+  public GeoServiceEventHandler(GeoServiceHelper geoServiceHelper) {
+    this.geoServiceHelper = geoServiceHelper;
+  }
+
+  @HandleBeforeCreate
+  public void handleBeforeCreate(GeoService geoService) throws Exception {
+    logger.info(
+        "Loading capabilities before creating geo service from URL: \"{}\"", geoService.getUrl());
+    geoServiceHelper.loadServiceCapabilities(geoService);
+  }
+
+  @HandleBeforeSave
+  public void handleGeoServiceSave(GeoService geoService) throws Exception {
+    logger.info(
+        "Loading capabilities before saving geo service from URL: \"{}\"", geoService.getUrl());
+    geoServiceHelper.loadServiceCapabilities(geoService);
+  }
+}
