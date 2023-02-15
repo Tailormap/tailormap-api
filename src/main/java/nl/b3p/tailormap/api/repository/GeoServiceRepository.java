cbb862e75316accf46f1aa278ff6839aa77330e7
Matthijs Laan <matthijsln@gmail.com>
2023-02-15 16:59:30 +0100

    Add permitAll()

--- src/main/java/nl/b3p/tailormap/api/repository/GeoServiceRepository.java
+++ src/main/java/nl/b3p/tailormap/api/repository/GeoServiceRepository.java
@@ -9,10 +9,17 @@ import nl.b3p.tailormap.api.persistence.GeoService;
 import nl.b3p.tailormap.api.security.annotation.PreAuthorizeAdmin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.Optional;
 
 @PreAuthorizeAdmin
 @RepositoryRestResource(
     path = "geo-services",
     collectionResourceRel = "geo-services",
     itemResourceRel = "geo-service")
-public interface GeoServiceRepository extends JpaRepository<GeoService, Long> {}
+public interface GeoServiceRepository extends JpaRepository<GeoService, Long> {
+  @PreAuthorize("permitAll()")
+  @Override
+  Optional<GeoService> findById(Long aLong);
+}
