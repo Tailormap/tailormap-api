cebf0a4d2a0ff3183e9c355167eec4d2f72511a0
Matthijs Laan <matthijsln@gmail.com>
2023-02-15 18:08:38 +0100

    Add permitAll()

--- src/main/java/nl/b3p/tailormap/api/repository/GeoServiceRepository.java
+++ src/main/java/nl/b3p/tailormap/api/repository/GeoServiceRepository.java
@@ -5,13 +5,12 @@
  */
 package nl.b3p.tailormap.api.repository;
 
+import java.util.Optional;
 import nl.b3p.tailormap.api.persistence.GeoService;
 import nl.b3p.tailormap.api.security.annotation.PreAuthorizeAdmin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
-
-import java.util.Optional;
 
 @PreAuthorizeAdmin
 @RepositoryRestResource(
