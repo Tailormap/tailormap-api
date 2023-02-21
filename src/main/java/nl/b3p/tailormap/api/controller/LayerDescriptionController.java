90672526c4c895333892039e05dc1f16cd641cff
Matthijs Laan <matthijsln@gmail.com>
2023-02-21 09:37:28 +0100

    Use ResponseStatusException

--- src/main/java/nl/b3p/tailormap/api/controller/LayerDescriptionController.java
+++ src/main/java/nl/b3p/tailormap/api/controller/LayerDescriptionController.java
@@ -8,7 +8,6 @@ package nl.b3p.tailormap.api.controller;
 import java.io.Serializable;
 import java.util.stream.Collectors;
 import nl.b3p.tailormap.api.annotation.AppRestController;
-import nl.b3p.tailormap.api.persistence.Application;
 import nl.b3p.tailormap.api.persistence.TMAttributeDescriptor;
 import nl.b3p.tailormap.api.persistence.TMFeatureType;
 import nl.b3p.tailormap.api.persistence.json.AppLayerRef;
@@ -17,7 +16,6 @@ import nl.b3p.tailormap.api.persistence.json.TMAttributeType;
 import nl.b3p.tailormap.api.persistence.json.TMGeometryType;
 import nl.b3p.tailormap.api.viewer.model.Attribute;
 import nl.b3p.tailormap.api.viewer.model.LayerDetails;
-import nl.b3p.tailormap.api.viewer.model.Service;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +23,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @AppRestController
 @Validated
@@ -35,18 +34,16 @@ public class LayerDescriptionController {
 
   @GetMapping
   public ResponseEntity<Serializable> getAppLayerDescription(
-      @ModelAttribute Application application,
       @ModelAttribute AppLayerRef ref,
-      @ModelAttribute Service service,
       @ModelAttribute GeoServiceLayer layer,
       @ModelAttribute TMFeatureType featureType) {
 
     if (layer == null) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Can't find app layer ref %s" + ref);
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Can't find app layer ref " + ref);
     }
 
     if (featureType == null) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Layer does not have feature type");
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Layer does not have feature type");
     }
 
     LayerDetails r =
