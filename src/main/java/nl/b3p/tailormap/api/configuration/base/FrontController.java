bef4dd84d551f36910a91ac5700206d3503f7404
Matthijs Laan <matthijsln@gmail.com>
2023-04-18 17:25:46 +0200

    [HTM-720] Add URLs to front controller

--- src/main/java/nl/b3p/tailormap/api/configuration/base/FrontController.java
+++ src/main/java/nl/b3p/tailormap/api/configuration/base/FrontController.java
@@ -6,23 +6,42 @@
 package nl.b3p.tailormap.api.configuration.base;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Front controller which forwards requests to URLs created by the Angular routing module to the
+ * proper viewer or admin index.html of the Angular bundle, for example when the user refreshes the
+ * page. Unfortunately, a proper front controller would first check if the file which the requested
+ * URL maps to exists and only return the index.html file if it doesn't. That is quite complicated
+ * to do in Spring Boot so as a poor-mans alternative we explicitly match the specific router URLs
+ * navigated to by the Angular frontends which don't map to files from the bundle.
+ *
+ * <p>It could happen that a frontend developer adds a route and forgets to update this front
+ * controller, and it is only noticed later when someone presses F5 and gets a 404.
+ */
 @Controller
 public class FrontController {
-  @RequestMapping(value = "/app/**")
+  @GetMapping(value = {"/login", "/app/**", "/service/**"})
   public String appIndex() {
-    // Return static /index.html for URLs made the by Angular frontend app routing module which all
-    // start with "/app/".
     return "forward:/index.html";
   }
 
-  @RequestMapping(value = {"/admin", "/admin/", "/admin/page/**"})
+  @GetMapping(
+      value = {
+        "/admin",
+        "/admin/login",
+        "/admin/catalog",
+        "/admin/catalog/**", // Extra line needed in order to match slashes. Regexps won't match.
+        "/admin/user**",
+        "/admin/group**",
+        "/admin/applications",
+        "/admin/applications/**"
+      })
   public String adminIndex() {
     return "forward:/admin/index.html";
   }
 
-  @RequestMapping(value = {"/swagger-ui", "/swagger-ui/"})
+  @GetMapping(value = {"/swagger-ui", "/swagger-ui/"})
   public String swaggerUiWelcomePage() {
     return "redirect:/swagger-ui/index.html";
   }
