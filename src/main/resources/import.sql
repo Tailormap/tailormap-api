6b031d6a6ddcaf7b85d6bd7024dab42c7da0226c
Matthijs <matthijsln@gmail.com>
2023-05-09 18:00:47 +0200

    Remove groups only used for authorization rules, no users should be assigned to them (#444)

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,5 @@
 
 insert into groups(name, system_group, description) values ('admin', true, 'Administrators with full access');
-insert into groups(name, system_group, description) values ('app-authenticated', true, 'Users authorized for application with authentication required');
 insert into groups(name, system_group, description) values ('actuator', true, 'Users authorized for Spring Boot Actuator (monitoring and management)');
 
 -- Default admin account will be created by AdminAccountCreator with a random password
