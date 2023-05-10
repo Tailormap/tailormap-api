1f04b1542baf61686f32a52927390af824865e16
Puck Meerburg <puck@puckipedia.com>
2023-05-10 12:43:19 +0000

    Add login screen configuration endpoint
    This allows for the viewer to show SSO login buttons, and even bypass the
    login screen entirely, if desired.

--- src/main/java/nl/b3p/tailormap/api/controller/UserController.java
+++ src/main/java/nl/b3p/tailormap/api/controller/UserController.java
@@ -7,7 +7,10 @@ package nl.b3p.tailormap.api.controller;
 
 import java.io.Serializable;
 import java.util.stream.Collectors;
+import nl.b3p.tailormap.api.viewer.model.LoginConfiguration;
+import nl.b3p.tailormap.api.viewer.model.LoginConfigurationSsoLinksInner;
 import nl.b3p.tailormap.api.viewer.model.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +18,15 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Provides user and login information */
 @RestController
 public class UserController {
+  @Autowired private ClientRegistrationRepository clientRegistrationRepository;
 
   public UserController() {}
 
@@ -44,5 +50,24 @@ public class UserController {
               .collect(Collectors.toSet()));
     }
     return ResponseEntity.status(HttpStatus.OK).body(userResponse);
+  }
+
+  @GetMapping(
+      path = "${tailormap-api.base-path}/login/configuration",
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  @SuppressWarnings("unchecked")
+  public ResponseEntity<LoginConfiguration> getSSOEndpoints() {
+    LoginConfiguration result = new LoginConfiguration();
+
+    if (clientRegistrationRepository instanceof Iterable<?>) {
+      for (ClientRegistration reg : (Iterable<ClientRegistration>) clientRegistrationRepository) {
+        result.addSsoLinksItem(
+            new LoginConfigurationSsoLinksInner()
+                .name(reg.getClientName())
+                .url("/api/oauth2/authorization/" + reg.getRegistrationId()));
+      }
+    }
+
+    return ResponseEntity.status(HttpStatus.OK).body(result);
   }
 }
