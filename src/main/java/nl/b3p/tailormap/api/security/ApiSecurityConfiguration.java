229f1bf76ef520c66ad1cc0a4f6120e842b9e724
Puck Meerburg <puck@puckipedia.com>
2023-06-29 10:09:09 +0000

    ApiSecurityConfiguration: handle redirecting the user back after SSO

--- src/main/java/nl/b3p/tailormap/api/security/ApiSecurityConfiguration.java
+++ src/main/java/nl/b3p/tailormap/api/security/ApiSecurityConfiguration.java
@@ -5,9 +5,13 @@
  */
 package nl.b3p.tailormap.api.security;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import nl.b3p.tailormap.api.persistence.Group;
 import nl.b3p.tailormap.api.repository.GroupRepository;
 import nl.b3p.tailormap.api.repository.OIDCConfigurationRepository;
@@ -18,13 +22,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -63,6 +71,40 @@ public class ApiSecurityConfiguration {
       http = http.csrf().csrfTokenRepository(csrfTokenRepository).and();
     }
 
+    // Before redirecting the user to the OAuth2 authorization endpoint, store the requested
+    // redirect URL.
+    RedirectStrategy redirectStrategy =
+        new DefaultRedirectStrategy() {
+          @Override
+          public void sendRedirect(
+              HttpServletRequest request, HttpServletResponse response, String url)
+              throws IOException {
+            request.getSession().setAttribute("redirectUrl", request.getParameter("redirectUrl"));
+            super.sendRedirect(request, response, url);
+          }
+        };
+
+    // When OAuth2 authentication succeeds, use the redirect URL stored in the session to send them
+    // back.
+    AuthenticationSuccessHandler authenticationSuccessHandler =
+        new AuthenticationSuccessHandler() {
+          public void onAuthenticationSuccess(
+              HttpServletRequest request,
+              HttpServletResponse response,
+              Authentication authentication)
+              throws IOException {
+            HttpSession session = request.getSession(false);
+            if (session != null) {
+              String redirectUrl = (String) session.getAttribute("redirectUrl");
+              if (redirectUrl != null) {
+                response.sendRedirect(redirectUrl);
+                return;
+              }
+            }
+            response.sendRedirect("/");
+          }
+        };
+
     http.securityMatchers(matchers -> matchers.requestMatchers(apiBasePath + "/**"))
         .authorizeHttpRequests(
             authorize ->
@@ -75,9 +117,13 @@ public class ApiSecurityConfiguration {
             login ->
                 login
                     .authorizationEndpoint(
-                        endpoint -> endpoint.baseUri(apiBasePath + "/oauth2/authorization"))
+                        endpoint ->
+                            endpoint
+                                .baseUri(apiBasePath + "/oauth2/authorization")
+                                .authorizationRedirectStrategy(redirectStrategy))
                     .redirectionEndpoint(
-                        endpoint -> endpoint.baseUri(apiBasePath + "/oauth2/callback")))
+                        endpoint -> endpoint.baseUri(apiBasePath + "/oauth2/callback"))
+                    .successHandler(authenticationSuccessHandler))
         .logout()
         .logoutUrl(apiBasePath + "/logout")
         .logoutSuccessHandler(
