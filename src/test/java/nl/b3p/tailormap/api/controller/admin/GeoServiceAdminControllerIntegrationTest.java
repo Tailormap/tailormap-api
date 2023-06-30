2625dd8399a91beeed11abd6bbc9195bda15470f
Matthijs Laan <matthijsln@gmail.com>
2023-06-30 11:01:07 +0200

    Add required title and refreshCapabilities properties

--- src/test/java/nl/b3p/tailormap/api/controller/admin/GeoServiceAdminControllerIntegrationTest.java
+++ src/test/java/nl/b3p/tailormap/api/controller/admin/GeoServiceAdminControllerIntegrationTest.java
@@ -24,6 +24,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -67,6 +68,8 @@ class GeoServiceAdminControllerIntegrationTest {
           new ObjectMapper()
               .createObjectNode()
               .put("protocol", "wms")
+              .put("title", "test")
+              .put("refreshCapabilities", true)
               .put("url", url)
               .toPrettyString();
 
