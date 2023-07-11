629db4a239c1316343b479a351d1df5b8952272e
Matthijs Laan <matthijsln@gmail.com>
2023-07-11 17:40:39 +0200

    Return only the root cause message (gives useful message for all 3 supported JDBC stores)

--- src/main/java/nl/b3p/tailormap/api/repository/validation/FeatureSourceValidator.java
+++ src/main/java/nl/b3p/tailormap/api/repository/validation/FeatureSourceValidator.java
@@ -6,6 +6,8 @@
 
 package nl.b3p.tailormap.api.repository.validation;
 
+import static nl.b3p.tailormap.api.util.TMExceptionUtils.joinAllThrowableMessages;
+
 import java.lang.invoke.MethodHandles;
 import java.net.URI;
 import java.net.URL;
@@ -19,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 @Component
@@ -44,7 +47,9 @@ public class FeatureSourceValidator implements Validator {
   }
 
   private void validateWFS(TMFeatureSource featureSource, Errors errors) {
-    if (errors.getFieldError("url") != null) {
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "url", "errors.required", "URL is required");
+
+    if (errors.hasErrors()) {
       return;
     }
 
@@ -52,11 +57,11 @@ public class FeatureSourceValidator implements Validator {
     try {
       uri = new URL(featureSource.getUrl()).toURI();
     } catch (Exception e) {
-      errors.rejectValue("url", "invalid", "Invalid URI");
+      errors.rejectValue("url", "errors.url.invalid", "Invalid URI");
       return;
     }
     if (!"https".equals(uri.getScheme()) && !"http".equals(uri.getScheme())) {
-      errors.rejectValue("url", "invalid-scheme", "Invalid URI scheme");
+      errors.rejectValue("url", "errors.url.invalid-scheme", "Invalid URI scheme");
       return;
     }
 
@@ -64,33 +69,48 @@ public class FeatureSourceValidator implements Validator {
       try {
         new WFSFeatureSourceHelper().loadCapabilities(featureSource);
       } catch (UnknownHostException e) {
-        errors.rejectValue("url", "unknown-host", "Unknown host: \"" + uri.getHost() + "\"");
+        errors.rejectValue("url", "errors.unknown-host", "Unknown host: \"" + uri.getHost() + "\"");
       } catch (Exception e) {
         String msg =
             String.format(
                 "Error loading WFS capabilities from URL \"%s\": %s",
-                featureSource.getUrl(), ExceptionUtils.getMessage(e));
+                featureSource.getUrl(), joinAllThrowableMessages(e));
         String loggerMsg =
-            " -- This may not be an application error but a problem with an external service or user-entered data.";
-        logger.info(msg + loggerMsg, e);
-        errors.rejectValue("url", "loading-capabilities-failed", msg);
+            "The following exception may not be an application error but could be a problem with an external service or user-entered data: ";
+        logger.info(loggerMsg + msg, e);
+        errors.rejectValue("url", "errors.loading-capabilities-failed", msg);
       }
     }
   }
 
   private void validateJDBC(TMFeatureSource featureSource, Errors errors) {
+    if (featureSource.getJdbcConnection() == null) {
+      errors.rejectValue(
+          "jdbcConnection", "errors.required", "JDBC connection properties are required");
+      return;
+    }
+    if (featureSource.getAuthentication() == null) {
+      errors.rejectValue(
+          "authentication", "errors.required", "Database username and password are required");
+      return;
+    }
+
+    if (errors.hasErrors()) {
+      return;
+    }
+
     if (featureSource.isRefreshCapabilities()) {
       try {
         new JDBCFeatureSourceHelper().loadCapabilities(featureSource);
       } catch (Exception e) {
         String msg =
             String.format(
-                "Error loading capabilities from JDBC datastore \"%s\": %s",
-                featureSource.getJdbcConnection(), ExceptionUtils.getMessage(e));
+                "Error loading capabilities from JDBC datastore: %s",
+                ExceptionUtils.getRootCauseMessage(e));
         String loggerMsg =
-            " -- This may not be an application error but a problem with an external service or user-entered data.";
-        logger.info(msg + loggerMsg, e);
-        errors.rejectValue("url", "loading-capabilities-failed", msg);
+            "The following exception may not be an application error but could be a problem with an external service or user-entered data: ";
+        logger.info(loggerMsg + msg, e);
+        errors.rejectValue("url", "errors.loading-capabilities-failed", msg);
       }
     }
   }
