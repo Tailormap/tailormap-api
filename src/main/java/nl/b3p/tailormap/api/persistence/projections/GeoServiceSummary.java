1f8d2cc96b66293cece7f6319ea9d5f420953fa0
Matthijs Laan <matthijsln@gmail.com>
2024-01-31 14:38:51 +0100

    HTM-999: Add summary projections to allow loading entire catalog

---
+++ src/main/java/nl/b3p/tailormap/api/persistence/projections/GeoServiceSummary.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2024 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package nl.b3p.tailormap.api.persistence.projections;
+
+import java.util.List;
+import nl.b3p.tailormap.api.persistence.GeoService;
+import nl.b3p.tailormap.api.persistence.json.GeoServiceLayer;
+import nl.b3p.tailormap.api.persistence.json.GeoServiceProtocol;
+import nl.b3p.tailormap.api.persistence.json.GeoServiceSettings;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(
+    name = "summary",
+    types = {GeoService.class})
+public interface GeoServiceSummary {
+  String getId();
+
+  GeoServiceProtocol getProtocol();
+
+  String getTitle();
+
+  List<GeoServiceLayer> getLayers();
+
+  GeoServiceSettings getSettings();
+}
---
+++ src/main/java/nl/b3p/tailormap/api/persistence/projections/TMFeatureSourceSummary.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2024 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package nl.b3p.tailormap.api.persistence.projections;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.List;
+import nl.b3p.tailormap.api.persistence.TMFeatureSource;
+import nl.b3p.tailormap.api.persistence.json.JDBCConnectionProperties;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(
+    name = "summary",
+    types = {TMFeatureSource.class})
+public interface TMFeatureSourceSummary {
+
+  Long getId();
+
+  TMFeatureSource.Protocol getProtocol();
+
+  @JsonIgnore
+  JDBCConnectionProperties getJdbcConnection();
+
+  default JDBCConnectionProperties.DbtypeEnum getDbType() {
+    return getJdbcConnection() == null ? null : getJdbcConnection().getDbtype();
+  }
+
+  String getTitle();
+
+  List<TMFeatureTypeSummary> getFeatureTypes();
+
+  interface TMFeatureTypeSummary {
+    Long getId();
+
+    String getName();
+
+    String getTitle();
+
+    boolean isWriteable();
+  }
+}
