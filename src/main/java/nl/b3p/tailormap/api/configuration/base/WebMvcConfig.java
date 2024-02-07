10cbd6ec19024452afbd400688773bf756cb4ed4
Matthijs Laan <matthijsln@gmail.com>
2024-02-07 16:47:33 +0100

    HTM-1007: Set no-cache Cache-Control for index.html

--- src/main/java/nl/b3p/tailormap/api/configuration/base/WebMvcConfig.java
+++ src/main/java/nl/b3p/tailormap/api/configuration/base/WebMvcConfig.java
@@ -8,6 +8,7 @@ package nl.b3p.tailormap.api.configuration.base;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,6 +28,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     registry
         .addResourceHandler("/*/index.html")
         .addResourceLocations(resourceLocations.split(",")[0])
+        // no-cache means the browser must revalidate index.html with a conditional HTTP request
+        // using If-Modified-Since. This is needed to always have the latest frontend loaded in the
+        // browser after deployment of a new release.
+        .setCacheControl(CacheControl.noCache())
         .resourceChain(true)
         .addTransformer(indexHtmlTransformer);
   }
