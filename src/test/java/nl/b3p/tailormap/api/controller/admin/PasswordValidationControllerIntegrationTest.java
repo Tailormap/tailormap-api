f845b8c23c6e98760b79d39218c28fc71cdd429c
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-03-22 11:57:10 +0100

    Fix other tests that need more context

--- src/test/java/nl/b3p/tailormap/api/controller/admin/PasswordValidationControllerIntegrationTest.java
+++ src/test/java/nl/b3p/tailormap/api/controller/admin/PasswordValidationControllerIntegrationTest.java
@@ -6,13 +6,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import nl.b3p.tailormap.api.annotation.PostgresIntegrationTest;
+import nl.b3p.tailormap.api.persistence.Group;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 /*
  * Copyright (C) 2023 B3Partners B.V.
@@ -21,8 +27,17 @@ import org.springframework.test.web.servlet.MockMvc;
  */
 @AutoConfigureMockMvc
 @PostgresIntegrationTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PasswordValidationControllerIntegrationTest {
-  @Autowired private MockMvc mockMvc;
+
+  @Autowired private WebApplicationContext context;
+
+  private MockMvc mockMvc;
+
+  @BeforeAll
+  void initialize() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+  }
 
   @Value("${tailormap-api.admin.base-path}")
   private String adminBasePath;
@@ -34,11 +49,15 @@ class PasswordValidationControllerIntegrationTest {
     "jI@#%09y,true",
     "very long password without special characters but unguessable,true"
   })
+  @WithMockUser(
+      username = "tm-admin",
+      authorities = {Group.ADMIN})
   @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
   void test(String password, String expected) throws Exception {
     mockMvc
         .perform(
             post(adminBasePath + "/validate-password")
+                //                    .servletPath(adminBasePath + "/validate-password")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
                 .param("password", password)
                 .accept(MediaType.APPLICATION_JSON))
