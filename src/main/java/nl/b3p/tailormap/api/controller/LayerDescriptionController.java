f9d54f53f2306127148ee6f0c3fe49e9adac346f
Matthijs <matthijsln@gmail.com>
2024-03-22 20:50:50 +0100

    Add forms (#764)
    * [HTM-1061] Fix anonymous user access and user login
    
    * Update integration testing to use the actual security configs
    
    * Add forms entity
    
    * Updated form entity, add options + fields
    
    * Allow setting form id to connect form to app layer
    
    * Update sql filename
    
    * Add form to viewer API, move to common-schemas.yaml
    
    * Remove unused
    
    ---------
    
    Co-authored-by: Mark Prins <1165786+mprins@users.noreply.github.com>
    Co-authored-by: Geert Plaisier <geert.plaisier@gnamic.nl>

--- src/main/java/nl/b3p/tailormap/api/controller/LayerDescriptionController.java
+++ src/main/java/nl/b3p/tailormap/api/controller/LayerDescriptionController.java
@@ -10,23 +10,24 @@ import static nl.b3p.tailormap.api.persistence.helper.TMFeatureTypeHelper.getCon
 
 import io.micrometer.core.annotation.Timed;
 import java.io.Serializable;
-import java.util.Optional;
 import java.util.Set;
 import nl.b3p.tailormap.api.annotation.AppRestController;
 import nl.b3p.tailormap.api.persistence.Application;
+import nl.b3p.tailormap.api.persistence.Form;
 import nl.b3p.tailormap.api.persistence.GeoService;
 import nl.b3p.tailormap.api.persistence.TMFeatureType;
 import nl.b3p.tailormap.api.persistence.helper.TMFeatureTypeHelper;
 import nl.b3p.tailormap.api.persistence.json.AppLayerSettings;
 import nl.b3p.tailormap.api.persistence.json.AppTreeLayerNode;
-import nl.b3p.tailormap.api.persistence.json.AttributeSettings;
 import nl.b3p.tailormap.api.persistence.json.GeoServiceLayer;
 import nl.b3p.tailormap.api.persistence.json.TMAttributeDescriptor;
 import nl.b3p.tailormap.api.persistence.json.TMAttributeType;
 import nl.b3p.tailormap.api.persistence.json.TMGeometryType;
 import nl.b3p.tailormap.api.repository.FeatureSourceRepository;
+import nl.b3p.tailormap.api.repository.FormRepository;
 import nl.b3p.tailormap.api.viewer.model.Attribute;
 import nl.b3p.tailormap.api.viewer.model.LayerDetails;
+import nl.b3p.tailormap.api.viewer.model.LayerDetailsForm;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -46,8 +47,12 @@ public class LayerDescriptionController {
 
   private final FeatureSourceRepository featureSourceRepository;
 
-  public LayerDescriptionController(FeatureSourceRepository featureSourceRepository) {
+  private final FormRepository formRepository;
+
+  public LayerDescriptionController(
+      FeatureSourceRepository featureSourceRepository, FormRepository formRepository) {
     this.featureSourceRepository = featureSourceRepository;
+    this.formRepository = formRepository;
   }
 
   @Transactional
@@ -86,6 +91,15 @@ public class LayerDescriptionController {
             .editable(TMFeatureTypeHelper.isEditable(application, appTreeLayerNode, tmft));
 
     AppLayerSettings appLayerSettings = application.getAppLayerSettings(appTreeLayerNode);
+
+    Form form;
+    if (appLayerSettings.getFormId() != null) {
+      form = formRepository.findById(appLayerSettings.getFormId()).orElse(null);
+      if (form != null) {
+        r.setForm(new LayerDetailsForm().options(form.getOptions()).fields(form.getFields()));
+      }
+    }
+
     Set<String> readOnlyAttributes =
         TMFeatureTypeHelper.getReadOnlyAttributes(tmft, appLayerSettings);
 
@@ -93,7 +107,6 @@ public class LayerDescriptionController {
         .map(
             pair -> {
               TMAttributeDescriptor a = pair.getLeft();
-              AttributeSettings settings = pair.getRight();
               return new Attribute()
                   .featureType(tmft.getId())
                   .key(a.getName())
@@ -105,11 +118,8 @@ public class LayerDescriptionController {
                   .editable(
                       !a.getName().equals(tmft.getPrimaryKeyAttribute())
                           && !readOnlyAttributes.contains(a.getName()))
-                  .editAlias(Optional.ofNullable(settings.getTitle()).orElse(a.getName()))
                   .defaultValue(a.getDefaultValue())
-                  .nullable(a.getNullable())
-                  .valueList(/*String[]*/ null /* TODO */)
-                  .allowValueListOnly(false /* TODO */);
+                  .nullable(a.getNullable());
             })
         .forEach(r::addAttributesItem);
 
