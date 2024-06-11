13695d3ee64c6a51767a62e9f28c3b201b7f28aa
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-06-11 17:08:49 +0200

    WIP [HTM-1076][HTM-1077] TMFeatureType event handler to propagate changes to solr indexes
    fixup after API changes in https://github.com/B3Partners/tailormap-api/pull/876
    
    move testcases and remove now useless test, use solr service inject
    
    fix rebase issues

--- src/main/java/org/tailormap/api/repository/ApplicationRepository.java
+++ src/main/java/org/tailormap/api/repository/ApplicationRepository.java
@@ -5,8 +5,11 @@
  */
 package org.tailormap.api.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.tailormap.api.persistence.Application;
@@ -21,4 +24,17 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
   @Override
   @NonNull
   Optional<Application> findById(@NonNull Long aLong);
+
+  /**
+   * Find all applications that have a layer that is linked to a specific (Solr) index.
+   *
+   * @param indexId The index id to search for
+   */
+  @NonNull
+  @PreAuthorize("permitAll()")
+  @Query(
+      value =
+          "select * from application app, lateral jsonb_path_query(app.settings, ('$.layerSettings.**{1}.searchIndexId ? (@ == '||:indexId||')')::jsonpath)",
+      nativeQuery = true)
+  List<Application> findByIndexId(@Param("indexId") @NonNull Long indexId);
 }
