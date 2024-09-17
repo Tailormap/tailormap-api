41e1725a9783448885697cfc5306b9b5c51ce300
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-09-17 17:17:35 +0200

    HTM-1220: initial (scheduled/background) task controller to determine the API

---
+++ src/test/java/org/tailormap/api/controller/admin/TaskAdminControllerIntegrationTest.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2024 B3Partners B.V.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+package org.tailormap.api.controller.admin;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junitpioneer.jupiter.Stopwatch;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.tailormap.api.annotation.PostgresIntegrationTest;
+import org.tailormap.api.persistence.Group;
+
+@AutoConfigureMockMvc
+@Stopwatch
+@PostgresIntegrationTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TaskAdminControllerIntegrationTest {
+  @Autowired private WebApplicationContext context;
+  private MockMvc mockMvc;
+
+  @Value("${tailormap-api.admin.base-path}")
+  private String adminBasePath;
+
+  @BeforeAll
+  void initialize() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+  }
+
+  @Test
+  @WithMockUser(
+      username = "tm-admin",
+      authorities = {Group.ADMIN})
+  @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+  void listAllTasks() throws Exception {
+    mockMvc
+        .perform(get(adminBasePath + "/tasks").accept(MediaType.APPLICATION_JSON))
+        //                .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.tasks").isArray())
+        .andExpect(jsonPath("$.tasks.length()").value(2));
+  }
+
+  @Test
+  @WithMockUser(
+      username = "tm-admin",
+      authorities = {Group.ADMIN})
+  @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+  void listTasksForType() throws Exception {
+    mockMvc
+        .perform(
+            get(adminBasePath + "/tasks")
+                .queryParam("type", "dummy")
+                .accept(MediaType.APPLICATION_JSON))
+        // .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.tasks").isArray())
+        .andExpect(jsonPath("$.tasks.length()").value(2));
+  }
+
+  @Test
+  @WithMockUser(
+      username = "tm-admin",
+      authorities = {Group.ADMIN})
+  @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+  void detailsOfTask() throws Exception {
+    mockMvc
+        .perform(
+            get(adminBasePath + "/tasks/{uuid}", "6308d26e-fe1e-4268-bb28-20db2cd06914")
+                .accept(MediaType.APPLICATION_JSON))
+        //                .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.uuid").value("6308d26e-fe1e-4268-bb28-20db2cd06914"))
+        .andExpect(jsonPath("$.type").value("dummy"))
+        .andExpect(jsonPath("$.status").value("running"))
+        .andExpect(jsonPath("$.progress").value(0.5));
+  }
+
+  @Test
+  @WithMockUser(
+      username = "tm-admin",
+      authorities = {Group.ADMIN})
+  @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+  void startTask() throws Exception {
+    mockMvc
+        .perform(
+            put(adminBasePath + "/tasks/{uuid}/start", "6308d26e-fe1e-4268-bb28-20db2cd06914")
+                .accept(MediaType.APPLICATION_JSON))
+        //                .andDo(print())
+        .andExpect(status().isAccepted());
+  }
+
+  @Test
+  @WithMockUser(
+      username = "tm-admin",
+      authorities = {Group.ADMIN})
+  @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+  void stopTask() throws Exception {
+    mockMvc
+        .perform(
+            put(adminBasePath + "/tasks/{uuid}/stop", "6308d26e-fe1e-4268-bb28-20db2cd06914")
+                .accept(MediaType.APPLICATION_JSON))
+        //                .andDo(print())
+        .andExpect(status().isAccepted());
+  }
+
+  @Test
+  @WithMockUser(
+      username = "tm-admin",
+      authorities = {Group.ADMIN})
+  @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+  void deleteTask() throws Exception {
+    mockMvc
+        .perform(
+            delete(adminBasePath + "/tasks/{uuid}", "6308d26e-fe1e-4268-bb28-20db2cd06914")
+                .accept(MediaType.APPLICATION_JSON))
+        //                .andDo(print())
+        .andExpect(status().isNoContent());
+  }
+}
