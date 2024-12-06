c4bb140d18a2ef4269066d90972645d095ab1587
Matthijs Laan <matthijsln@gmail.com>
2024-12-06 13:45:27 +0100

    Add expectations

--- src/test/java/org/tailormap/api/controller/PageControllerIntegrationTest.java
+++ src/test/java/org/tailormap/api/controller/PageControllerIntegrationTest.java
@@ -6,16 +6,23 @@
 
 package org.tailormap.api.controller;
 
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.startsWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.tailormap.api.TestRequestProcessor.setServletPath;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.tailormap.api.annotation.PostgresIntegrationTest;
 
@@ -35,8 +42,42 @@ class PageControllerIntegrationTest {
   }
 
   @Test
-  void te1stHomePage() throws Exception {
+  void testHomePageWithFilteredTile() throws Exception {
     final String url = apiBasePath + "/page";
-    mockMvc.perform(get(url).with(setServletPath(url))).andExpect(status().isOk());
+    mockMvc
+        .perform(get(url).accept(MediaType.APPLICATION_JSON).with(setServletPath(url)))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.name").value("home"))
+        .andExpect(jsonPath("$.tiles.length()").value(Matchers.greaterThan(3)))
+        .andExpect(jsonPath("$.tiles[?(@.applicationUrl == '/app/default')]").exists())
+        .andExpect(
+            jsonPath(
+                "$.tiles[?(@.title == 'Default app')].image",
+                everyItem(startsWith("http://localhost/api/uploads/portal-image/"))))
+        .andExpect(jsonPath("$.tiles[?(@.title == 'About')].pageUrl").value("/page/about"))
+        .andExpect(
+            jsonPath(
+                    "$.tiles[?(@.applicationUrl == '/app/secured' && @.title == 'Secured app (unfiltered)')]")
+                .exists())
+        .andExpect(
+            jsonPath("$.tiles[?(@.applicationUrl == '/app/secured' && @.title == 'Secured app')]")
+                .doesNotExist());
+  }
+
+  @Test
+  @WithMockUser(
+      username = "tm-admin",
+      authorities = {"admin"})
+  void testFilteredTileShownWhenAuthorized() throws Exception {
+    final String url = apiBasePath + "/page";
+    mockMvc
+        .perform(get(url).accept(MediaType.APPLICATION_JSON).with(setServletPath(url)))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.name").value("home"))
+        .andExpect(
+            jsonPath("$.tiles[?(@.applicationUrl == '/app/secured' && @.title == 'Secured app')]")
+                .exists());
   }
 }
