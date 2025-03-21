c1e40026cbb4fbb65f3906308d962b01b9b305ef
Matthijs Laan <matthijsln@gmail.com>
2025-03-21 10:17:35 +0100

    Use pattern-matching instanceof

--- src/main/java/org/tailormap/api/controller/EditFeatureController.java
+++ src/main/java/org/tailormap/api/controller/EditFeatureController.java
@@ -145,10 +145,9 @@ public class EditFeatureController implements Constants {
         simpleFeature.setAttribute(entry.getKey(), entry.getValue());
       }
 
-      if (fs instanceof SimpleFeatureStore) {
-        ((SimpleFeatureStore) fs).setTransaction(transaction);
-        List<FeatureId> newFids =
-            ((SimpleFeatureStore) fs).addFeatures(DataUtilities.collection(simpleFeature));
+      if (fs instanceof SimpleFeatureStore simpleFeatureStore) {
+        simpleFeatureStore.setTransaction(transaction);
+        List<FeatureId> newFids = simpleFeatureStore.addFeatures(DataUtilities.collection(simpleFeature));
 
         transaction.commit();
         // find the created feature to return
@@ -203,16 +202,15 @@ public class EditFeatureController implements Constants {
       fs = featureSourceFactoryHelper.openGeoToolsFeatureSource(tmFeatureType);
       // find feature to update
       final Filter filter = ff.id(ff.featureId(fid));
-      if (!fs.getFeatures(filter).isEmpty() && fs instanceof SimpleFeatureStore) {
+      if (!fs.getFeatures(filter).isEmpty() && fs instanceof SimpleFeatureStore simpleFeatureStore) {
         handleGeometryAttributesInput(
             tmFeatureType, appLayerSettings, partialFeature, attributesMap, application, fs);
         // NOTE geotools does not report back that the feature was updated, no error === success
-        ((SimpleFeatureStore) fs).setTransaction(transaction);
-        ((SimpleFeatureStore) fs)
-            .modifyFeatures(
-                attributesMap.keySet().toArray(new String[] {}),
-                attributesMap.values().toArray(),
-                filter);
+        simpleFeatureStore.setTransaction(transaction);
+        simpleFeatureStore.modifyFeatures(
+            attributesMap.keySet().toArray(new String[] {}),
+            attributesMap.values().toArray(),
+            filter);
         transaction.commit();
         // find the updated feature to return
         patchedFeature = getFeature(fs, filter, application);
