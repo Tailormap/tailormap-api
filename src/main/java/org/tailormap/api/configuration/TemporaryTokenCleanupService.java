20adfc0b8444c145eb45c0cc0debd9ff41591a2a
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-09-24 13:06:21 +0200

    Use a jdbctemplate an not a repository for the expired token cleanup thread

--- src/main/java/org/tailormap/api/configuration/TemporaryTokenCleanupService.java
+++ src/main/java/org/tailormap/api/configuration/TemporaryTokenCleanupService.java
@@ -11,22 +11,36 @@ import java.time.ZoneId;
 import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.tailormap.api.repository.TemporaryTokenRepository;
 
+/**
+ * Service for cleaning up expired temporary tokens from the database. This class uses
+ * {@link org.springframework.jdbc.core.simple.JdbcClient} to execute the cleanup operation as we are not allowed to
+ * inject a {@code Repository} in the scheduled method and when autowired into the class it does not provide a
+ * transaction manager in the started thread.
+ */
 @Service
 @Transactional
 public class TemporaryTokenCleanupService {
   private static final Logger logger =
       LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private final JdbcClient jdbcClient;
+
+  public TemporaryTokenCleanupService(JdbcClient jdbcClient) {
+    this.jdbcClient = jdbcClient;
+  }
 
   @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES, initialDelay = 1)
-  public void cleanExpiredTokens(TemporaryTokenRepository temporaryTokenRepository) {
+  public void cleanExpiredTokens() {
     logger.trace("Running expired token cleanup...");
-    long deletedCount =
-        temporaryTokenRepository.deleteByExpirationTimeIsBefore(OffsetDateTime.now(ZoneId.systemDefault()));
+    long deletedCount = this.jdbcClient
+        .sql("DELETE FROM temporary_token WHERE expiration_time < ?")
+        .param(OffsetDateTime.now(ZoneId.systemDefault()))
+        .update();
+
     logger.trace("Cleaned up {} tokens", deletedCount);
   }
 }
