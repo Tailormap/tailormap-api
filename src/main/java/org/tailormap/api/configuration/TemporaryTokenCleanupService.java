50337821f04f5607d278aba50e101f6f6cd74f97
Matthijs Laan <matthijsln@gmail.com>
2025-10-16 14:59:56 +0200

    Use JPA repository in token cleanup

--- src/main/java/org/tailormap/api/configuration/TemporaryTokenCleanupService.java
+++ src/main/java/org/tailormap/api/configuration/TemporaryTokenCleanupService.java
@@ -6,41 +6,31 @@
 package org.tailormap.api.configuration;
 
 import java.lang.invoke.MethodHandles;
-import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.tailormap.api.repository.TemporaryTokenRepository;
 
-/**
- * Service for cleaning up expired temporary tokens from the database. This class uses
- * {@link org.springframework.jdbc.core.simple.JdbcClient} to execute the cleanup operation as we are not allowed to
- * inject a {@code Repository} in the scheduled method and when autowired into the class it does not provide a
- * transaction manager in the started thread.
- */
 @Service
-@Transactional
 public class TemporaryTokenCleanupService {
   private static final Logger logger =
       LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-  private final JdbcClient jdbcClient;
 
-  public TemporaryTokenCleanupService(JdbcClient jdbcClient) {
-    this.jdbcClient = jdbcClient;
+  private final TemporaryTokenRepository temporaryTokenRepository;
+
+  public TemporaryTokenCleanupService(TemporaryTokenRepository temporaryTokenRepository) {
+    this.temporaryTokenRepository = temporaryTokenRepository;
   }
 
   @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES, initialDelay = 1)
+  @Transactional
   public void cleanExpiredTokens() {
     logger.trace("Running expired token cleanup...");
-    long deletedCount = this.jdbcClient
-        .sql("DELETE FROM temporary_token WHERE expiration_time < ?")
-        .param(OffsetDateTime.now(ZoneId.of("UTC")))
-        .update();
-
-    logger.trace("Cleaned up {} tokens", deletedCount);
+    temporaryTokenRepository.deleteByExpirationTimeBefore(ZonedDateTime.now(ZoneId.of("UTC")));
   }
 }
