1339fd955cc25bedbd53ddb12ae1ab2a04ba3e84
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-10-20 19:33:37 +0200

    use static method th get the authentication

--- src/main/java/org/tailormap/api/persistence/listener/AdminRevisionListener.java
+++ src/main/java/org/tailormap/api/persistence/listener/AdminRevisionListener.java
@@ -6,35 +6,27 @@
 package org.tailormap.api.persistence.listener;
 
 import jakarta.persistence.PrePersist;
+import java.lang.invoke.MethodHandles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.tailormap.api.persistence.AdminRevisionEntity;
 
-import java.lang.invoke.MethodHandles;
-import java.util.Optional;
-
 @Component
 public class AdminRevisionListener {
-    private static final Logger logger =
-            LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-    private final Optional<Authentication> authentication;
-
-   public AdminRevisionListener(Optional<Authentication> authentication) {
-       // we may need to use SecurityContextHolder.getContext().getAuthentication(); instead
-    this.authentication = authentication;
-  }
+  private static final Logger logger =
+      LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   @PrePersist
   public void prePersist(AdminRevisionEntity entity) {
-
-  if (authentication.isEmpty()) {
-        logger.warn("No authentication info available, cannot set modifiedBy on revision entity");
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null) {
+      logger.warn("No authentication info available, cannot set modifiedBy on revision entity");
       return;
     }
-      logger.info("Updating revision entity, with authentication info from: {}", authentication.get());
-    entity.setModifiedBy(authentication.get().getName());
+    logger.debug("Updating revision entity, with authentication info from: {}", authentication);
+    entity.setModifiedBy(authentication.getName());
   }
 }
