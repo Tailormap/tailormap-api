d0a8cfc9fe6f023f7f96fb30ddb2f707043f7598
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-10-21 16:37:26 +0200

    Add auditing to Form, update test config

--- src/main/java/org/tailormap/api/repository/FormRepository.java
+++ src/main/java/org/tailormap/api/repository/FormRepository.java
@@ -7,8 +7,9 @@
 package org.tailormap.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.tailormap.api.persistence.Form;
 
 @RepositoryRestResource(path = "forms", collectionResourceRel = "forms", itemResourceRel = "form")
-public interface FormRepository extends JpaRepository<Form, Long> {}
+public interface FormRepository extends JpaRepository<Form, Long>, RevisionRepository<Form, Long, Long> {}
