9401099c09927aab7351cccf775744bcd69c64d5
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-11-06 14:43:16 +0100

    HTM-1736 | HTM-1737: API and code to manage attachments
    - add
    - list
    - get
    - delete

--- src/main/java/org/tailormap/api/controller/AttachmentsController.java
+++ src/main/java/org/tailormap/api/controller/AttachmentsController.java
@@ -9,7 +9,9 @@ import jakarta.validation.Valid;
 import java.io.IOException;
 import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
+import java.nio.ByteBuffer;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import org.geotools.api.data.Query;
@@ -92,6 +94,8 @@ public class AttachmentsController {
 
     TMFeatureType tmFeatureType = editUtil.getEditableFeatureType(application, appTreeLayerNode, service, layer);
 
+    checkFeatureExists(tmFeatureType, featureId);
+
     Set<@Valid AttachmentAttributeType> attachmentAttrSet =
         tmFeatureType.getSettings().getAttachmentAttributes();
     if (attachmentAttrSet == null || attachmentAttrSet.isEmpty()) {
@@ -115,10 +119,6 @@ public class AttachmentsController {
                 + fileData.length));
     logger.debug("Using attachment attribute {}", attachmentAttributeType);
 
-    if (!checkFeatureExists(tmFeatureType, featureId)) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Feature with id " + featureId + " does not exist");
-    }
-
     AttachmentMetadata response;
     try {
       response = AttachmentsHelper.insertAttachment(tmFeatureType, attachment, featureId, fileData);
@@ -129,38 +129,133 @@ public class AttachmentsController {
     return new ResponseEntity<>(response, HttpStatus.CREATED);
   }
 
-  @DeleteMapping(path = "${tailormap-api.base-path}/attachment/{attachmentId}")
-  public ResponseEntity<Serializable> deleteAttachment(@PathVariable UUID attachmentId) {
+  /**
+   * Add an attachment to a feature
+   *
+   * @param appTreeLayerNode the application tree layer node
+   * @param service the geo service
+   * @param layer the geo service layer
+   * @param application the application
+   * @param featureId the feature id
+   * @return the response entity
+   */
+  @GetMapping(
+      path = {
+        "${tailormap-api.base-path}/{viewerKind}/{viewerName}/layer/{appLayerId}/feature/{featureId}/attachment"
+      },
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  @Transactional
+  public ResponseEntity<List<AttachmentMetadata>> listAttachments(
+      @ModelAttribute AppTreeLayerNode appTreeLayerNode,
+      @ModelAttribute GeoService service,
+      @ModelAttribute GeoServiceLayer layer,
+      @ModelAttribute Application application,
+      @PathVariable String featureId) {
+
+    TMFeatureType tmFeatureType = editUtil.getEditableFeatureType(application, appTreeLayerNode, service, layer);
+
+    checkFeatureExists(tmFeatureType, featureId);
+    checkFeatureTypeSupportsAttachments(tmFeatureType);
+
+    List<AttachmentMetadata> response;
+    try {
+      response = AttachmentsHelper.listAttachmentsForFeature(tmFeatureType, featureId);
+    } catch (IOException | SQLException e) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
+
+  @DeleteMapping(
+      path = "${tailormap-api.base-path}/{viewerKind}/{viewerName}/layer/{appLayerId}/attachment/{attachmentId}")
+  public ResponseEntity<Serializable> deleteAttachment(
+      @ModelAttribute AppTreeLayerNode appTreeLayerNode,
+      @ModelAttribute GeoService service,
+      @ModelAttribute GeoServiceLayer layer,
+      @ModelAttribute Application application,
+      @PathVariable UUID attachmentId) {
     editUtil.checkEditAuthorisation();
 
-    logger.debug("TODO: Deleting attachment with id {}", attachmentId);
-    throw new UnsupportedOperationException("Not implemented yet");
+    TMFeatureType tmFeatureType = editUtil.getEditableFeatureType(application, appTreeLayerNode, service, layer);
+
+    checkFeatureTypeSupportsAttachments(tmFeatureType);
+
+    try {
+      AttachmentsHelper.deleteAttachment(attachmentId, tmFeatureType);
+    } catch (IOException | SQLException e) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
+  @Transactional
   @GetMapping(
-      path = "${tailormap-api.base-path}/attachment/{attachmentId}",
+      path = "${tailormap-api.base-path}/{viewerKind}/{viewerName}/layer/{appLayerId}/attachment/{attachmentId}",
       produces = {"application/octet-stream"})
-  // TODO determine return type: ResponseEntity<byte[]> or ResponseEntity<InputStreamResource>?
-  public ResponseEntity<byte[]> getAttachment(@PathVariable UUID attachmentId) {
-    logger.debug("TODO: Getting attachment with id {}", attachmentId);
+  public ResponseEntity<byte[]> getAttachment(
+      @ModelAttribute AppTreeLayerNode appTreeLayerNode,
+      @ModelAttribute GeoService service,
+      @ModelAttribute GeoServiceLayer layer,
+      @ModelAttribute Application application,
+      @PathVariable UUID attachmentId) {
 
-    throw new UnsupportedOperationException("Not implemented yet");
+    TMFeatureType tmFeatureType = editUtil.getEditableFeatureType(application, appTreeLayerNode, service, layer);
+
+    try {
+      final AttachmentsHelper.AttachmentWithBinary attachmentWithBinary =
+          AttachmentsHelper.getAttachment(tmFeatureType, attachmentId);
+
+      if (attachmentWithBinary == null) {
+        throw new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "Attachment %s not found".formatted(attachmentId.toString()));
+      }
+
+      // the binary attachment() is a read-only ByteBuffer, so we cant use .array()
+      final ByteBuffer bb = attachmentWithBinary.attachment().asReadOnlyBuffer();
+      bb.rewind();
+      byte[] attachmentData = new byte[bb.remaining()];
+      bb.get(attachmentData);
+
+      return ResponseEntity.ok()
+          .header(
+              "Content-Disposition",
+              "inline; filename=\""
+                  + attachmentWithBinary.attachmentMetadata().getFileName() + "\"")
+          .contentType(MediaType.parseMediaType(
+              attachmentWithBinary.attachmentMetadata().getMimeType()))
+          .body(attachmentData);
+    } catch (SQLException | IOException e) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
   }
 
-  private boolean checkFeatureExists(TMFeatureType tmFeatureType, String featureId) {
+  private void checkFeatureExists(TMFeatureType tmFeatureType, String featureId) throws ResponseStatusException {
     final Filter fidFilter = ff.id(ff.featureId(featureId));
     SimpleFeatureSource fs = null;
     try {
       fs = featureSourceFactoryHelper.openGeoToolsFeatureSource(tmFeatureType);
       Query query = new Query();
       query.setFilter(fidFilter);
-      return fs.getCount(query) > 0;
+      if (fs.getCount(query) < 1) {
+        throw new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "Feature with id " + featureId + " does not exist");
+      }
     } catch (IOException e) {
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     } finally {
       if (fs != null) {
         fs.getDataStore().dispose();
       }
+    }
+  }
+
+  private void checkFeatureTypeSupportsAttachments(TMFeatureType tmFeatureType) throws ResponseStatusException {
+    Set<@Valid AttachmentAttributeType> attachmentAttrSet =
+        tmFeatureType.getSettings().getAttachmentAttributes();
+    if (attachmentAttrSet == null || attachmentAttrSet.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Feature type does not support attachments");
     }
   }
 }
