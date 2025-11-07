ef00f91541c15de0923a8aabac5863c991981e32
steinkobben <93925365+steinkobben@users.noreply.github.com>
2025-11-07 15:16:31 +0100

    HTM-1760: use list of editable attributes instead of read only attributes in feature type settings (#1467)
    * make not editable standard and use list of editable attributes
    
    * create flyway script to replace readOnlyAttributes with editableAttributes in featureTypeSettings
    
    * adapt check for editable in attribute to editableAttributes list
    
    * add attributes to editableAttributes in populate testdata
    
    * add attributes to editableAttributes in populate testdata in featuretypes used in tests
    
    * bugfix: getEditableAttributes should remove readonly attributes fom layer settings
    
    * add geometry attribute to editable attributes
    
    * add all geometry attributes to editable attributes

--- src/main/java/org/tailormap/api/persistence/helper/TMFeatureTypeHelper.java
+++ src/main/java/org/tailormap/api/persistence/helper/TMFeatureTypeHelper.java
@@ -51,12 +51,13 @@ public class TMFeatureTypeHelper {
     return hiddenAttributes;
   }
 
-  public static Set<String> getReadOnlyAttributes(
+  public static Set<String> getEditableAttributes(
       @NotNull TMFeatureType featureType, @NotNull AppLayerSettings appLayerSettings) {
-    Set<String> readOnlyAttributes = new HashSet<>();
-    Optional.ofNullable(featureType.getSettings().getReadOnlyAttributes()).ifPresent(readOnlyAttributes::addAll);
-    Optional.ofNullable(appLayerSettings.getReadOnlyAttributes()).ifPresent(readOnlyAttributes::addAll);
-    return readOnlyAttributes;
+    Set<String> editableAttributes = new HashSet<>();
+    Optional.ofNullable(featureType.getSettings().getEditableAttributes()).ifPresent(editableAttributes::addAll);
+    Optional.ofNullable(appLayerSettings.getReadOnlyAttributes()).ifPresent(editableAttributes::removeAll);
+    Optional.ofNullable(featureType.getAllGeometryAttributeNames()).ifPresent(editableAttributes::addAll);
+    return editableAttributes;
   }
 
   public record AttributeWithSettings(TMAttributeDescriptor attributeDescriptor, AttributeSettings settings) {}
