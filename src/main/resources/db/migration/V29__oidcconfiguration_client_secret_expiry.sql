8388c60fc4aeac16d76f49e59a913579f9dc587e
Matthijs Laan <matthijsln@gmail.com>
2026-05-04 19:58:32 +0200

    Also add column to history schema

--- src/main/resources/db/migration/V29__oidcconfiguration_client_secret_expiry.sql
+++ src/main/resources/db/migration/V29__oidcconfiguration_client_secret_expiry.sql
@@ -1,2 +1,5 @@
     alter table if exists oidcconfiguration
        add column client_secret_expiry date;
+
+    alter table history.oidcconfiguration_revisions
+        add column client_secret_expiry date;
